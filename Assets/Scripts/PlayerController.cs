2e9b0af359fb290ca58d3a7bc4ecb27a4809a91c
noodles885 <55859706+noodles885@users.noreply.github.com>
2023-09-21 17:41:23 -0300

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,46 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
-    public float vertspeed = 0;
-    private Rigidbody rb;
 
-    private float movementX;
-    private float movementY;
-    
-    // Start is called before the first frame update
-    void Start()
+ private Rigidbody rb; 
+
+
+ private float movementX;
+ private float movementY;
+
+
+ public float speed = 0; 
+
+
+ void Start()
     {
+
         rb = GetComponent<Rigidbody>();
     }
+ 
 
-    private void OnMove(InputValue movementValue)
+ void OnMove(InputValue movementValue)
     {
+
         Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
     }
 
-    private void FixedUpdate()
+
+ private void FixedUpdate() 
     {
-        Vector3 movement = new Vector3(movementX,0.0f ,movementY);
-        
 
-        rb.AddForce(movement * speed);
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
 
 
-        
-        float moveVertical = Input.GetAxis("Vertical");
-        float moveHeight = Input.GetAxis("Jump2");
-        
-        Vector3 vertmovement = new Vector3(0.0f, moveHeight, 0.0f);
-        rb.AddForce(movement * speed);
-        rb.AddForce(vertmovement * vertspeed);
+        rb.AddForce(movement * speed); 
     }
+
+ void OnTriggerEnter(Collider other) 
+    {
+ 
+ if (other.gameObject.CompareTag("PickUp")) 
+        {
+ 
+            other.gameObject.SetActive(false);
+        }
+    }
+
 
 }
