454f7bf2f8ac6f4a7a0285afb307afb706ce67df
noodles885 <55859706+noodles885@users.noreply.github.com>
2023-09-22 13:52:31 -0300

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 0;
+    public float jumpForce = 5.0f; 
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
@@ -16,61 +17,65 @@ public class PlayerController : MonoBehaviour
     private int count;
     private float movementX;
     private float movementY;
-    
-    
-    void Start() {
-        
+    private bool isGrounded; 
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
         count = 0;
-
         SetCountText();
         winTextObject.SetActive(false);
-
+        Vector3 jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
-    void OnMove(InputValue movementValue) {
-
+    void OnMove(InputValue movementValue)
+    {
         Vector2 movementVector = movementValue.Get<Vector2>();
-
         movementX = movementVector.x;
         movementY = movementVector.y;
-
     }
 
-    public void SetCountText() {
-
+    public void SetCountText()
+    {
         countText.text = "Points: " + count.ToString() + "/12";
-        if (count >= 12) {
-
+        if (count >= 12)
+        {
             winTextObject.SetActive(true);
             Time.timeScale = 0f;
-            
         }
-
     }
 
-    private void FixedUpdate() {
-        
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.2f, movementY);
 
         rb.AddForce(movement * speed);
 
-    }
-
-    void OnTriggerEnter(Collider other) {
-
-        if (other.gameObject.CompareTag("PickUp")) {
-
-            other.gameObject.SetActive(false);
-            count ++;
-            SetCountText();
-
-        }
         
-
+        isGrounded = true;
+        
+        if (isGrounded && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            
+            Jump();
+            isGrounded = false;
+        }
     }
 
+    void Jump()
+    {
+        // Apply an upward force to make the player jump
+        rb.AddForce(Jump * jumpForce, ForceMode.Impulse);
+    }
 
-
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
 
 }
